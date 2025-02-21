1a6d69748f9f30ace4191dea55578f1c34431392
Carroll42 <ford6313@gmail.com>
2025-02-21 19:01:19 +0900

    add player controller movement

--- Assets/Scripts/UI/Joystick/UI_Joystick.cs
+++ Assets/Scripts/UI/Joystick/UI_Joystick.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class UI_Joystick : MonoBehaviour
+public class UI_Joystick : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    [SerializeField]
+    Image _background;
+
+    [SerializeField]
+    Image _Handler;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
